16b874c8f0727cec6e337a8d3b463352bef8d513
Anette Rivera <anettem.rivera@gmail.com>
2020-05-14 00:30:25 +0000

    Automatically backed up by Learn

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -29,10 +29,10 @@ class String
   end
 
   def count_sentences
-    if self.split(/[.!?]/).length
-    else
-    self.split(/[.!?]/).map{|x| !(x.match(/\w+/).nil?)}.reject{|x| x == false}.size 
-  end
+    self.split(/[.!?]/).length
+  #   else
+  #   self.split(/[.!?]/).map{|x| !(x.match(/\w+/).nil?)}.reject{|x| x == false}.size 
+  # end
   end
   
 end
