7d5c2742bd78c2a1b09001663a2bb7096741f047
Anette Rivera <anettem.rivera@gmail.com>
2020-05-14 00:47:29 +0000

    Automatically backed up by Learn

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -29,8 +29,8 @@ class String
   end
 
   def count_sentences
-    sentence = []
-    sentence = self.split(/[.!?]/).length 
+    
+    self.split(/[.!?]/).length 
     sentence.reject { |s| s.empty? }
 
   end
